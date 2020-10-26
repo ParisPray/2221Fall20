9e61b7fc9392e92fff124f9e2440f3c8ae817f01
Paris Pray <paris.pray@gmail.com>
2020-10-25 23:17:00 -0600

    Added Shooting and Death and Respawn

---
+++ Death and Respawn Sandbox/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class PlayerController : MonoBehaviour
+{
+    private CharacterController controller;
+    private Vector3 movement, lookDirection;
+    private float gravity = -9.81f, yAxisVar;
+    public float speed, normalSpeed, fastSpeed, jumpForce;
+    public int jumpMax;
+    private int jumpCount;
+    
+
+    private void Start()
+    {
+        controller = GetComponent<CharacterController>();
+        speed = normalSpeed;
+    }
+
+    private void Update()
+    {
+        yAxisVar += gravity * Time.deltaTime;
+
+        if (controller.isGrounded && movement.y < 0)
+        {
+            yAxisVar = -1;
+            jumpCount = 0;
+        }
+
+        if (Input.GetButtonDown("Jump") && jumpCount < jumpMax)
+        {
+            yAxisVar = jumpForce;
+            jumpCount++;
+        }
+        
+        var verticalInput = Input.GetAxis("Vertical");
+        var horizontalInput = Input.GetAxis("Horizontal");
+
+        if (Input.GetButtonDown("Fire3"))
+        {
+            speed = fastSpeed;
+        }
+        else if (Input.GetButtonUp("Fire3"))
+        {
+            speed = normalSpeed;
+        }
+
+        lookDirection.Set(horizontalInput, 0, verticalInput);
+
+        if (lookDirection == Vector3.zero)
+        {
+            lookDirection.Set(0.0001f, 0, 0.0001f);
+        }
+        
+        if (horizontalInput > 0.5f || horizontalInput < -0.5f ||verticalInput > 0.5f || verticalInput < -0.5f)
+        {
+            transform.rotation = Quaternion.LookRotation(lookDirection);
+        }
+
+        movement.Set(horizontalInput, yAxisVar, verticalInput);
+        controller.Move(movement * (speed * Time.deltaTime));
+    }
+}
